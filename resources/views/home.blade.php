7d32320759d0a9fab162e6aca0db97466b364d7e
ya-zeed <63609245+ya-zeed@users.noreply.github.com>
2021-07-24 00:10:23 +0300

    second Algm

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.app')
+<link rel="stylesheet" href="css/homeStyle.css">
 
 @section('content')
 <div class="container">
@@ -15,30 +16,89 @@
                     @endif
 
                     @if ($isStart)
-                        <table class="table table-hover" id="reports" style="direction: rtl">
-                            <thead class="thead-light">
-                                <tr>
-                                    <th style="width: 10%">#</th>
-                                    <th style="width: 40%">اليحياوي</th>
-                                    <th style="width: 40%">النقاط</th>
-                                    <th style="width: 10%">إقصاء</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($users as $user)
-                                    <tr>
-                                        <td>{{$user->id}}</td>
-                                        <td>{{$user->fakename}}</td>
-                                        <td>{{$user->score}}</td>
-                                        <td></td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
+                    <div class="circle2"></div>
+    <div class="circle1"></div>
+    <img src="imgs/logo.png" class="myBg">
+    <div class="col-12 p-0 rounded rounded-lg-0 my-2" id="myTable">
+        <div id="table_header" class="row text-center d-flex align-content-center" dir="rtl">
+            <div class="col-2">#</div>
+            <div class="col-5">اليحياوي</div>
+            <div class="col-2">النقاط</div>
+            <div class="col-3">اقصاء</div>
+        </div>
+
+        <!-- لوب هنا يا وحش -->
+        <div id="table_row" class="row text-center d-flex align-content-center" dir="rtl">
+            <div class="col-2">1</div>
+            <div class="col-5">يزييديددديددد</div>
+            <div class="col-2">45</div>
+            <div class="col-3"><button type="button" class="btn btn-primary btn-sm" data-toggle="modal"
+                    data-target="#exampleModal" data-whatever="selectedName" style="background-color: #b5def5; color: black; border: none;">اقصاء</button></div>
+                                                {{-- data-whatever -> Fake Name selected --}}
+        </div>
+        
+        <!-- Modal "ما عليك منها لا تلمسها الا اذا تبي تعدل الفورم" -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body" dir="rtl">
+                        <form> 
+                            <div class="form-group text-right" dir="rtl">
+                                <label for="recipient-name" class="col-form-label">المتهم:</label>
+                                <input type="text" class="form-control" id="selectedId" name="fakeId" disabled>
+                            </div>
+                            <div class="form-group text-right">
+                                <label for="message-text" class="col-form-label">اسمه:</label>
+                                <select name="nameId" class="form-control">
+                                    <option value="1">يزيد الطويل</option>
+                                    <option value="2">مجيد</option>
+                                    <option value="3">سارة الطويل</option>
+                                    <option value="4">يزيد الطويل</option>
+                                </select>
+                                {{-- loop for all users --}}
+                            </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer" dir="rtl">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">الغاء</button>
+                        <button type="button" class="btn btn-primary">اتهام</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
+        crossorigin="anonymous"></script>
+
+    <script>
+        $('#exampleModal').on('show.bs.modal', function (event) {
+            var button = $(event.relatedTarget)
+            var recipient = button.data('whatever') 
+            var modal = $(this)
+            modal.find('.modal-title').text('New message to ' + recipient)
+            modal.find('.modal-body input').val(recipient)
+        })
+    </script>
                     @else
-                        <div class="alert alert-warning waiting-members">
-                            <h1>هلا باليحياوي! ننتظر باقي المتسابقين</h1>
-                        </div>
+                    <div class="circle2"></div>
+                    <div class="circle1"></div>
+                    <img src="imgs/logo.png" class="myBg">
+                    <div class="col-12 offset-1 align-self-center p-2 rounded rounded-lg-0 mt-3 ml-0">
+                        <h2 class="d-flex d-sm-flex justify-content-center text-info font-weight-light mb-2"><img class="d-flex justify-content-sm-center" src="imgs/logo.png" style="height: 200px;"></h2>
+                        <h2 class="text-center text-white">هلا باليحياوي! ننتظر باقي المتسابقين</h2>
+                    </div>
                     @endif
                 </div>
             </div>
