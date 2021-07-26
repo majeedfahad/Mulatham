94e9ddfca9fb3add584ae799ed67fd3889cb6c65
Majeed <aabdulmajeed16@gmail.com>
2021-07-26 21:38:45 +0300

    solve duplicate entry issue

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,8 @@ use App\Models\AnswerUser;
 use App\Models\Question;
 use App\Models\Setting;
 use App\Models\User;
+use Exception;
+use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
@@ -46,21 +48,29 @@ class HomeController extends Controller
 
     public function answerQuestion(Request $request, $question_id)
     {
-        $user = Auth::user();
-        $answer = new AnswerUser();
-        $answer->user_id = $user->id;
-        $answer->question_id = $question_id;
+        try {
+            $user = Auth::user();
+            $answer = new AnswerUser();
+            $answer->user_id = $user->id;
+            $answer->question_id = $question_id;
 
-         
-        if($request['answer']) $answer->answer = $request['answer'];
+            
+            if($request['answer']) $answer->answer = $request['answer'];
 
-        $answer->answer;
-        $answer->answer_id = $request['answer'] ? null : $request['selectedAnswer'];
+            $answer->answer;
+            $answer->answer_id = $request['answer'] ? null : $request['selectedAnswer'];
+            
+            $answer->save();
+
+            $answer->assignScore($question_id);
+            return redirect()->route('home')->with(['success' => 'فالك الاجابة الصح']);
+        } catch (QueryException $e){
+            $errorCode = $e->errorInfo[1];
+            if($errorCode == 1062){
+                return redirect()->route('home')->with(['success' => 'وصلت إجابتك، فالك التوفيق']);
+            }
+        }
         
-        $answer->save();
-
-        $answer->assignScore($question_id);
-        return redirect()->route('home')->with(['success' => 'فالك الاجابة الصح']);
 
     }
 }
